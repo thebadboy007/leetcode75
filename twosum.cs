9a3747c434f039156000537e5f1bdcadf6f954cc
thebadboy007 <44163717+thebadboy007@users.noreply.github.com>
2025-08-21 21:13:12 +0000

    Implement TwoSum solution with main method and logic for finding indices

---
+++ twosum.cs
@@ -0,0 +1,26 @@
+public class TwoSum{
+    public static void Main(string[] args)
+    {
+        int nums[] = {2,5,7,8,10};
+        int target = 9;
+        int result = twoSum(nums, target);
+        Console.Writeline($"Indexes:{result[0]},{result[1]}");
+
+        public static twoSum(int nums[], int target)
+        {
+            var numToInsert =  new Dictonary<int int>();
+
+            for(int i=0; i< nums.length; i++){
+                int complement = target - nums[i];
+
+                if(numToInsert.TryGetValue(complement, out int index))
+                {
+                    return new int[]{index, i};
+
+                }
+                numToInsert[nums[i]] = i;
+            }
+            throw new ArgumentException("No Two sum solution");
+        }
+    }
+}
